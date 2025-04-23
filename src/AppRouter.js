cc6e21d55c3d74ed1f57aa674c2dcab72d3279b2
Lovelotus1st <lovelotusist@hotmail.com>
2025-04-23 18:03:33 -0400

    more changes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import WeatherApp from "./components/Weather/WeatherApp";
 import MovieApp from "./components/Movies/MovieApp";
 
 const AppRouter = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-      <Route path="/" element={<Navigate to="/movies" replace />} />
-        <Route path="/weather" element={<WeatherApp />} />
-        <Route path="/movies" element={<MovieApp />} />
-      </Routes>
-    </Router>
-  );
-};
+    return (
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/weather" replace />} />
+          <Route path="/weather" element={<WeatherApp />} />
+          <Route path="/movies" element={<MovieApp />} />
+        </Routes>
+      </Router>
+    );
+  };
 
 export default AppRouter;
